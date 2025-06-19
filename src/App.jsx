9afa6fb864bf56f132c26320b531fed42a11e98f
Matias Wilder <48178322@est.ort.edu.ar>
2025-06-18 21:00:08 -0300

    mensaje error y css terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [img, setimg] = useState(null);
+  const [img, setImg] = useState(null);
+  const [error, setError] = useState(null);
 
-  function ObtenerFoto(){
-      axios.get('https://api.thecatapi.com/v1/images/search')
-      .then(function (response) {
-      setimg(response.data[0].url);
-      console.log(response);
-    })
-      .catch(function (error) {
-      console.log(error.message);
-    })
+  function ObtenerFoto() {
+    axios.get('https://api.thecatapi.com/v1/images/search')
+      .then((response) => {
+        setImg(response.data[0].url);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error(error.message);
+        setImg(null);
+        setError('Hubo un error. No hay imágen');
+      });
   }
 
   return (
     <div>
       <h1>Fotos de gatos</h1>
       <button onClick={ObtenerFoto}>Nueva foto</button>
-      {}
-      <img src={img}></img>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {img && <img src={img} alt="gatito" />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
